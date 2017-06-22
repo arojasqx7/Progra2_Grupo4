a819bb189ccfded127446ff4d840ec77040a879e
andrey.rojas <andrey.rojas@sapiensdev.com>
2017-06-21 22:59:35 -0600

    Commit - Menu And Login Interface

---
+++ ProyectoGrupo4/Login.vb
@@ -0,0 +1,10 @@
+﻿Public Class Login
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim tab = New Menu()
+        tab.Show()
+    End Sub
+End Class
